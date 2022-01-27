c421720579420f604f996bf8a431cca078304602
webmasterdro <pdro.lima@pm.me>
2022-01-27 14:48:57 -0300

    feat: add authentication usecase

---
+++ domain/usecases/authentication.ts
@@ -0,0 +1,13 @@
+import { AccountModel } from "../models/account-model";
+
+type AuthenticationParams = {
+    email: string
+    password: string
+}
+
+/**
+ * Criamos um model, pois caso a API mude, nao precisamos alterar o nosso caso de uso.
+ */
+export interface Authentication {
+    auth (params: AuthenticationParams): Promise<AccountModel>
+}
