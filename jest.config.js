be470893e1841fb3bbaa07f66046468be0302059
webmasterdro <pdro.lima@pm.me>
2022-01-28 07:51:44 -0300

    style: eslint fixes

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
-      '!**/*.d.ts',
+    '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
   testPathIgnorePatterns: [
@@ -10,11 +10,11 @@ module.exports = {
   ],
   testEnvironment: 'jsdom',
   transform: {
-    '.+\\.(ts|tsx)$': 'ts-jest',
+    '.+\\.(ts|tsx)$': 'ts-jest'
   },
   moduleNameMapper: {
     '@/tests/(.*)': '<rootDir>/tests/$1',
     '@/(.*)': '<rootDir>/src/$1',
-    '\\.scss$': 'identity-obj-proxy',
+    '\\.scss$': 'identity-obj-proxy'
   }
 }
