2e79d10a311ac06d4693d79dfcb3602334631baa
webmasterdro <pdro.lima@pm.me>
2022-01-28 06:45:16 -0300

    chore: config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
-    entry: './src/main/index.tsx',
-    output: {
-        path: path.join(__dirname, 'public/js'),
-        publicPath: '/public/js',
-        filename: "bundle.js"
+  mode: 'development',
+  entry: './src/main/index.tsx',
+  output: {
+    path: path.join(__dirname, 'public/js'),
+    publicPath: '/public/js',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  devServer: {
+    devMiddleware: {
+      writeToDisk: true
     },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-        }
+    static: {
+      directory: path.join(__dirname, 'public')
     },
-    devServer: {
-        devMiddleware: {
-            writeToDisk: true
-        },
-        static: {
-            directory: path.join(__dirname, 'public'),
-        },
-        historyApiFallback: true,
-        port: 9090,
-    },
-    externals: {
-        'react': 'React',
-        'react-dom': 'ReactDOM',
-    },
-    plugins: [
-        new CleanWebpackPlugin()
-    ],
+    historyApiFallback: true,
+    port: 9090
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 }
