61a97cd3445194494594b9edd75efde9a293144c
webmasterdro <pdro.lima@pm.me>
2022-01-28 05:36:38 -0300

    fix: fake a more realistic faker value

--- src/infra/http/test/mock-axios.ts
+++ src/infra/http/test/mock-axios.ts
@@ -3,6 +3,6 @@ import faker from '@faker-js/faker'
 
 export const mockAxios = (): jest.Mocked<typeof axios> => {
   const mockedAxios = axios as jest.Mocked<typeof axios>
-  mockedAxios.post.mockResolvedValue({ data: faker.random.randomWords(100), status: faker.random.randomWords(100) })
+  mockedAxios.post.mockResolvedValue({ data: faker.random.randomWords(100), status: faker.datatype.number })
   return mockedAxios
 }
